5009e5e5dec6ed604608ee19018955dbdd06e117
FoXeRSiDe <grustota@mail.ru>
2024-09-13 03:48:20 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#1st program
+print(9**0.5*5)
+
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3st program            2 варианта написания
+#если нужны будут дополнения
+                            #кратко
+a=2*2+2
+b=2*(2+2)                   #print(2 * 2 + 2)
+print(a)                    #print(2 * (2 + 2))
+print(b)                    #print(2 * 2 + 2 == 2 * (2 + 2))
+print(bool(a==b))
+
+#4st program            не понял задачу по этому написал в двух вариантах
+a = '123.456'
+print(a[4])             #a = float('123.456')
+a = float(a)*10         #a = int(num * 10) % 10
+print(a)                #print(a)
+print(int(a)%10)
