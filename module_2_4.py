57cf410adbe20af0ac2c22de40cc0bcebfacb806
FoXeRSiDe <grustota@mail.ru>
2024-09-17 22:08:35 +0400

    module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -8,7 +8,7 @@ for i in numbers:
         for j in range(2, int(i ** 0.5) + 1):
             if i % j == 0:
                 is_prime = False
-                
+                break
         if is_prime:
             primes.append(i)
         else:
