524622de53f58886a222ddd86278d29494173429
FoXeRSiDe <grustota@mail.ru>
2024-09-23 12:45:39 +0400

    module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -3,14 +3,11 @@ import random
 def password(n):
     result = ""
     for i in range(1, 21):
-        for j in range(1, 21):
-            if i != j:
-                sum = i + j
-                if n % sum == 0:
-                    result += str(i) + str(j)
+        for j in range(i + 1, 21):
+            if n % (i + j) == 0:
+                result += str(i) + str(j)
     return result
 
-n = 4        #random.randint(3, 20)
+n = random.randint(3, 20)
 print(f"Число из первой вставки: {n}")
 print(f"Пароль: {password(n)}")
-#Выводит ответ и отзеркаливает
